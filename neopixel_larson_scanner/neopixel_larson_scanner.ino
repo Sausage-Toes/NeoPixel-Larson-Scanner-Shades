c39432fb7c7aed5f55c353ac2681ce55b8d45d5a
Sausage-Toes <sausage.toos@gmail.com>
2015-09-13 22:56:47 -0500

    Added color vars

--- neopixel_larson_scanner/neopixel_larson_scanner.ino
+++ neopixel_larson_scanner/neopixel_larson_scanner.ino
@@ -1,10 +1,15 @@
 #include <Adafruit_NeoPixel.h>
 
-#define PIN 1
-#define NUM_LED 20
+#define PIN 6
+#define NUM_LED 7
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LED, PIN, NEO_GRB + NEO_KHZ800);
 const uint32_t OFF = strip.Color(0, 0, 0);
+int r = 64;
+int g = 16;
+int b = 8;
+int wait = 100;
+
 
 void setup() {
   strip.begin();
@@ -21,16 +26,20 @@ void loop() {
  
   // Draw 5 pixels centered on pos.  setPixelColor() will clip any
   // pixels off the ends of the strip, we don't need to watch for that.
-  strip.setPixelColor(pos - 2, 0x100000); // Dark red
-  strip.setPixelColor(pos - 1, 0x800000); // Medium red
-  //strip.setPixelColor(pos - 1, 0x100000); // Medium red
-  strip.setPixelColor(pos    , 0xFF3000); // Center pixel is brightest
-  strip.setPixelColor(pos + 1, 0x800000); // Medium red
-  //strip.setPixelColor(pos + 1, 0x100000); // Medium red
-  strip.setPixelColor(pos + 2, 0x100000); // Dark red
- 
+//  strip.setPixelColor(pos - 2, 0x100000); // Dark red
+//  strip.setPixelColor(pos - 1, 0x800000); // Medium red
+//  strip.setPixelColor(pos    , 0xFF3000); // Center pixel is brightest
+//  strip.setPixelColor(pos + 1, 0x800000); // Medium red
+//  strip.setPixelColor(pos + 2, 0x100000); // Dark red
+
+  strip.setPixelColor(pos - 2, r*0.1,g*0.1,b*0.1); // Dark
+  strip.setPixelColor(pos - 1, r*0.6,g*0.6,b*0.6); // Medium 
+  strip.setPixelColor(pos    , r,g,b); // Center pixel is brightest
+  strip.setPixelColor(pos + 1, r*0.6,g*0.6,b*0.6); // Medium 
+  strip.setPixelColor(pos + 2, r*0.1,g*0.1,b*0.1); // Dark 
+
   strip.show();
-  delay(50);
+  delay(wait);
  
   // Rather than being sneaky and erasing just the tail pixel,
   // it's easier to erase it all and draw a new one next time.
@@ -49,10 +58,10 @@ void loop() {
 
 void SetAllPixelsToOff()
 {
-  for (uint8_t i = 0; i < 64; i++)
+  for (uint8_t i = 0; i < NUM_LED; i++)
   {
     strip.setPixelColor(i, OFF);
   }
   strip.show();
 }
-
+  
